25232801df3df0f2f981a9d0048ccee55bfc8fa6
yyyylhq <yyyylhq@163.com>
2024-03-14 11:50:23 +0800

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   model.py

--- model.py
+++ model.py
@@ -10,24 +10,27 @@ class TransformerEncoderLayer(nn.Module):
 
     @nn.compact
     def __call__(self, x, train=True):
-        linear0 = nn.Dense(self.d_model)
-        attention = nn.MultiHeadAttention(num_heads=self.nhead, qkv_features=self.d_model)
-        dropout0 = nn.Dropout(self.dropout_rate, deterministic=not train)
-        norm0 = nn.LayerNorm()
-        norm1 = nn.LayerNorm()
-        linear1 = nn.Dense(self.dim_feedfoward)
-        linear2= nn.Dense(self.d_model)
-        dropout1 = nn.Dropout(self.dropout_rate, deterministic=not train)
-        dropout2 = nn.Dropout(self.dropout_rate, deterministic=not train)
-        activation = nn.activation.relu
+        x = nn.Dense(self.d_model)(x)
+        x = nn.relu(x)
+        x = nn.MultiHeadAttention(num_heads=self.nhead, qkv_features=self.d_model)(x)
+        x = x + nn.Dropout(self.dropout_rate, deterministic=not train)(x)
+        x = nn.LayerNorm()(x)
 
-        x = linear0(x)
-        x = attention(x)
-        x = x + dropout0(x)
-        x = norm0(x)
+        x = nn.Dense(self.dim_feedfoward)(x)
+        x = nn.relu(x)
+        x = nn.Dropout(self.dropout_rate, deterministic=not train)(x)
 
-        x = x + dropout2(linear2(dropout1(activation(linear1(x)))))
-        x = norm1(x)
+        x = nn.Dense(self.d_model)(x)
+        x = x + nn.Dropout(self.dropout_rate, deterministic=not train)(x)
+
+        x = nn.LayerNorm()(x)
+
+        #x = attention(x)
+        #x = x + dropout0(x)
+        #x = norm0(x)
+
+        #x = x + dropout2(linear2(dropout1(activation(linear1(x)))))
+        #x = norm1(x)
 
         return x
 
@@ -50,18 +53,17 @@ class TransformerEncoder(nn.Module):
 
         return x
 
-m = TransformerEncoder()
-x = jnp.ones((1, 10, 404))
-params = m.init({'params': jax.random.key(0), 'dropout': jax.random.key(1)}, x)
+if __name__ == "__main__":
+    m = TransformerEncoder()
+    x = jnp.ones((1, 10, 404))
+    params = m.init({'params': jax.random.key(0), 'dropout': jax.random.key(1)}, x)
 
-"""
-for i in list(params):
-    for j in list(params[i]):
-        for k in list(params[i][j]):
-            for l in list(params[i][j][k]):
-                print(i, j, k, l)
-"""
+    for i in list(params):
+        for j in list(params[i]):
+            for k in list(params[i][j]):
+                for l in list(params[i][j][k]):
+                    print(i, j, k, l)
 
-x = jnp.ones((8, 10, 404))
-out = m.apply(params, x, train=True, rngs={'dropout': jax.random.key(0)})
-#print(m.tabulate({'params': jax.random.key(0), 'dropout': jax.random.key(1)}, jnp.ones((1, 100, 404)), compute_flops=True, compute_vjp_flops=False))
+    x = jnp.ones((8, 10, 404))
+    out = m.apply(params, x, train=True, rngs={'dropout': jax.random.key(0)})
+    #print(m.tabulate({'params': jax.random.key(0), 'dropout': jax.random.key(1)}, jnp.ones((1, 100, 404)), compute_flops=True, compute_vjp_flops=False))
